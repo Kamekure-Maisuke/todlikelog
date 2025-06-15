d1b211fc365233ad59698d9ea209a1413f8c41c5
kamekuramaisuke <taisukeoono350@gmail.com>
2025-06-15 11:04:55 +0900

    add report taskw

---
+++ report.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -eu
+
+now=$(date '+%Y%m')
+last=$(echo $now "-1" | bc)
+
+target=$(find data -type f | grep "$last")
+
+function showReport(){
+  echo "$now"のレポート
+  echo ""
+
+  cut -d ' ' -f2 $target |
+  sort |
+  uniq -c | 
+  sort -r |
+  awk '
+  {
+    print NR"位", $2 "(" $1 "回)"
+  }
+  '
+}
+
+showReport > report/"$last"
